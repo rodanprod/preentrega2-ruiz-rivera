958b4a081fcb1d59e69cc1c0ea19b5e0b93d6b4e
Rodrigo Ruiz Rivera <rodrigoruizrivera@MacBook-Pro-de-Rodrigo-2.local>
2024-10-24 23:03:35 -0300

    Updates

---
+++ precache-manifest.d0f9f8af25f051a1c4c85f05dcbe1da7.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "071908b46dab9a1860fc2aa693cfba6b",
+    "url": "/preentrega2-ruiz-rivera/index.html"
+  },
+  {
+    "revision": "d21387b4075f0ccb2c43",
+    "url": "/preentrega2-ruiz-rivera/static/css/2.b71989ea.chunk.css"
+  },
+  {
+    "revision": "284e3625870222aa11a2",
+    "url": "/preentrega2-ruiz-rivera/static/css/main.59a04be9.chunk.css"
+  },
+  {
+    "revision": "d21387b4075f0ccb2c43",
+    "url": "/preentrega2-ruiz-rivera/static/js/2.607414da.chunk.js"
+  },
+  {
+    "revision": "cb156245ee93a85eed33",
+    "url": "/preentrega2-ruiz-rivera/static/js/3.44b60ca4.chunk.js"
+  },
+  {
+    "revision": "284e3625870222aa11a2",
+    "url": "/preentrega2-ruiz-rivera/static/js/main.4d4e90e7.chunk.js"
+  },
+  {
+    "revision": "f0ef363006131987b2f5",
+    "url": "/preentrega2-ruiz-rivera/static/js/runtime~main.5ea46c42.js"
+  },
+  {
+    "revision": "6e69a3dbbd172ed41b073e1e2b7bfd15",
+    "url": "/preentrega2-ruiz-rivera/static/media/hyper.6e69a3db.png"
+  },
+  {
+    "revision": "a916ec31228ac51ea17832e9b699e61f",
+    "url": "/preentrega2-ruiz-rivera/static/media/rtx3080.a916ec31.png"
+  },
+  {
+    "revision": "e02f9017256fcc88e1e6420cc9d8daf1",
+    "url": "/preentrega2-ruiz-rivera/static/media/zephyrus.e02f9017.png"
+  }
+]);
